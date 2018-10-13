8e7b28f97fe31e2219221a0ef8d075a72ddd897c
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-10-13 00:34:09 -0300

    refactoring

--- examples/lab1.js
+++ examples/lab1.js
@@ -2,10 +2,23 @@
 
 const moduleTest = require("../lib");
 
+var context = {};
+context.getConfig = function(){
+    return {
+        db_url : "mongodb://localhost:27017",
+        db_username : "",
+        db_password : "",
+        db_name: "security",
+        db_collection_name: "role",
+        db_fieldRoleName: "_id",
+        db_fieldPermissionsName : "permissions"
+    };
+}
+
 var message1 = {};
 message1.roles = ["papel1","papel2"];
 message1.permission = "@functions-io-labs/math.sum";
-moduleTest(message1).then(function(result){
+moduleTest(message1, context).then(function(result){
     console.log("sucess! ", result);
 }, function(err){
     console.log("err! ", err);
--- test/test.js
+++ test/test.js
@@ -3,11 +3,24 @@
 const assert = require("assert");
 const moduleTest = require("../");
 
+var context = {};
+context.getConfig = function(){
+    return {
+        db_url : "mongodb://localhost:27017",
+        db_username : "",
+        db_password : "",
+        db_name: "security",
+        db_collection_name: "role",
+        db_fieldRoleName: "_id",
+        db_fieldPermissionsName : "permissions"
+    };
+}
+
 var message1 = {};
 message1.roles = ["papel1","papel2"];
 message1.permission = "@functions-io-labs/math.sum";
 
-moduleTest(message1).then(function(result){
+moduleTest(message1, context).then(function(result){
     assert.strictEqual(result, 1);
 }, function(err){
     assert.strictEqual(err, null);
