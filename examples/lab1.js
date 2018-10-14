320636409e85839c4d27ccaae7cfa844ffcfb91f
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-10-14 20:10:26 -0300

    refactoring

--- examples/lab1.js
+++ examples/lab1.js
@@ -1,25 +1,32 @@
 "use strict";
 
 const moduleTest = require("../lib");
+const driver = require("@functions-io-modules/datasource.driver.mongo");
 
 var context = {};
 context.getConfig = function(){
     return {
-        db_url : "mongodb://localhost:27017",
-        db_username : "",
-        db_password : "",
-        db_name: "security",
         db_collection_name: "role",
         db_fieldRoleName: "_id",
         db_fieldPermissionsName : "permissions"
     };
 }
 
+context.getDataSource = function(){
+    let config = {};
+    config.url = "mongodb://localhost:27017";
+    config.dataBaseName = "security";
+    return driver.getDataSource(config);
+}
+
+
 var message1 = {};
 message1.roles = ["papel1","papel2"];
 message1.permission = "@functions-io-labs/math.sum";
 moduleTest(message1, context).then(function(result){
     console.log("sucess! ", result);
+    driver.close();
 }, function(err){
     console.log("err! ", err);
+    driver.close();
 })
--- test/test.js
+++ test/test.js
@@ -2,18 +2,22 @@
 
 const assert = require("assert");
 const moduleTest = require("../");
+const driver = require("@functions-io-modules/datasource.driver.mongo");
 
 var context = {};
 context.getConfig = function(){
     return {
-        db_url : "mongodb://localhost:27017",
-        db_username : "",
-        db_password : "",
-        db_name: "security",
         db_collection_name: "role",
         db_fieldRoleName: "_id",
         db_fieldPermissionsName : "permissions"
     };
+}
+
+context.getDataSource = function(){
+    let config = {};
+    config.url = "mongodb://localhost:27017";
+    config.dataBaseName = "security";
+    return driver.getDataSource(config);
 }
 
 var message1 = {};
@@ -21,7 +25,17 @@ message1.roles = ["papel1","papel2"];
 message1.permission = "@functions-io-labs/math.sum";
 
 moduleTest(message1, context).then(function(result){
-    assert.strictEqual(result, 1);
+    try {
+        assert.strictEqual(result, true);
+    }
+    finally{
+        driver.close();
+    }
 }, function(err){
-    assert.strictEqual(err, null);
+    try {
+        assert.strictEqual(err, null);
+    }
+    finally{
+        driver.close();
+    }
 })
